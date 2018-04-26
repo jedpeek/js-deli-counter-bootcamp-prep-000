d28e03e881c26e9818e37997455634d6cd9a48db
Jed Peek <jedpeek@yahoo.com>
2018-04-26 23:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//var katzDeli = [];
-//var katzDeliLine = [];
+var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
@@ -8,9 +8,9 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 
 function nowServing(){
   if(katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine.shift()}`
+    return 'There is nobody waiting to be served!'
   }
   else{
-      return "The line is currently empty."
+    return `Currently serving ${katzDeliLine.shift()}`
   }
 }
