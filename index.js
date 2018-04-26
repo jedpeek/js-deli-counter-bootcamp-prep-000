5103a7cb5ad2bd00b37b1001b6109b24ba6dcd6b
Jed Peek <jedpeek@yahoo.com>
2018-04-26 23:52:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine.shift()}`
+    return `Currently serving ${katzDeliLine.shift()}.`
   
   }
   else{
