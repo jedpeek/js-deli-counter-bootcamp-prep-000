dfdbdf958da38b858ee178a61dcd56131cb3c563
Jed Peek <jedpeek@yahoo.com>
2018-04-26 23:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 
 function nowServing(){
   if(katzDeliLine.length > 0){
-  return katzDeliLine.shift();
+  return `Currently serving ${katzDeliLine.shift()}`;
   }
   else{
     return 'There is nobody waiting tobe served!'
