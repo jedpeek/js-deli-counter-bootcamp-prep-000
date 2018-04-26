74ed13900c39e175995babe02a16755c1ab49a91
Jed Peek <jedpeek@yahoo.com>
2018-04-26 23:45:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length > 0){
     var arr = [];
     for(var i = 0; i < katzDeliLine.length; i++){
-      arr.push(`${i + 1}. ${katzDeliLine[i]}, `)
+      arr.push(`${i + 1}. ${katzDeliLine[i]}`)
     }
    return `The line is currently: ${arr}`
   }
