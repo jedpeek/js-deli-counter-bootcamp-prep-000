0020553d2ad970a17f25fc350219c8a228b7161f
Jed Peek <jedpeek@yahoo.com>
2018-04-26 23:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(){
+  return katzDeliLine.pop();
+}
